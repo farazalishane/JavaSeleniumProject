c3650655ae9a8e721dd4d14b82c75fc579f138b9
Faraaz <shaneali1212@gmail.com>
2024-05-17 08:41:02 +0530

    First Commit for this new project

---
+++ src/main/java/inetBankingV1/inetBankingV1/ORMConfig.java
@@ -0,0 +1,7 @@
+package inetBankingV1.inetBankingV1;
+
+import at.chrl.orm.hibernate.configuration.JPAConfig;
+
+public class ORMConfig extends JPAConfig {
+
+}
---
+++ src/main/java/inetBankingV1/inetBankingV1/SpringConfig.java
@@ -0,0 +1,15 @@
+package inetBankingV1.inetBankingV1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import at.chrl.orm.hibernate.configuration.JPAConfig;
+
+@Configuration
+public class SpringConfig {
+
+	@Bean
+	public JPAConfig getJPAConfig(){
+		return new ORMConfig();
+	}
+}
